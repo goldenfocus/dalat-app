d1ef76820acf776955378a6541ef88a0a7eab4a5
Yan Lovez <yan@veganz.net>
2026-08-30 08:32:23 -0400

    chore: bump service worker to 1.0.9

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
 //
 // IMPORTANT: Update SW_VERSION when deploying new features
 // This triggers the update flow for all users
-const SW_VERSION = '1.0.8';
+const SW_VERSION = '1.0.9';
 
 const APP_URL = self.location.origin;
 
